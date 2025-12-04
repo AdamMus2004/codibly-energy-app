6599feba10b8a40e83529cbf6e6be71552fe6870
Adam Mus <adammuspraca@gmail.com>
2025-12-04 15:14:47 +0100

    Add AppConfig class

---
+++ src/main/java/com/example/backend/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.example.backend.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+
+        restTemplate().getMessageConverters().clear();
+        restTemplate().getMessageConverters().add(converter);
+        return restTemplate();
+    }
+}
